c207a26c899dd4e2a417ab318e5ba415460ab6d4
Maciej Koziol <maciejkoziol@Mac.hsd1.il.comcast.net>
2025-11-16 12:11:55 -0600

    Fix image 404 errors by including basePath in image src URLs

--- components/WorkCard/index.js
+++ components/WorkCard/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 const WorkCard = ({ img, name, description, onClick }) => {
+
   return (
     <div
       className="overflow-hidden rounded-lg p-2 laptop:p-4 first:ml-0 link"
@@ -13,7 +14,7 @@ const WorkCard = ({ img, name, description, onClick }) => {
         <img
           alt={name}
           className="h-full w-full object-cover hover:scale-110 transition-all ease-out duration-300"
-          src={img}
+         src={"/Personal-Portfolio" + img}
         ></img>
       </div>
       <h1 className="mt-5 text-3xl font-medium">
