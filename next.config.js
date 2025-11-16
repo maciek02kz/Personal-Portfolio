9565db3b8ebec465e4c53ef95db6927cd885b547
Maciej Koziol <maciejkoziol@Mac.hsd1.il.comcast.net>
2025-11-16 11:13:49 -0600

    Prepare for GitHub Pages deployment: add next config, build output

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  basePath: '/Personal-Portfolio',
+  assetPrefix: '/Personal-Portfolio/',
+  images: {
+    unoptimized: true,
+  },
 }
 
 module.exports = nextConfig
